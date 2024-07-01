3aba0da66cce8e26168ebc178013622ace395161
Guilherme Henrique <a226881906@fumec.edu.br>
2024-07-01 07:19:07 -0300

    Build: Implementing the ChangePasswordService

---
+++ src/services/ChangePasswordService.ts
@@ -0,0 +1,48 @@
+/* Imports:
+================================================================================*/
+    import dotenv from 'dotenv';
+    import { SupabaseService } from './SupabaseService';
+
+/* Setting Dotenv:
+================================================================================*/
+    dotenv.config();    
+
+/* Class:
+================================================================================*/
+    export class ChangePasswordService {
+        /* Global Variables:
+        ========================================================================*/
+            private supabase: any;
+            private supabaseService!: SupabaseService;
+
+        /* Constructor:
+        ========================================================================*/
+            constructor() { 
+                this.supabaseService = SupabaseService.getInstance();
+                this.supabase = this.supabaseService.createdClient;
+            }
+
+        /* Singleton Instance for Class:
+        ========================================================================*/
+            private static instance: ChangePasswordService;
+        
+            static getInstance() {
+                if (!ChangePasswordService.instance) {
+                    ChangePasswordService.instance = new ChangePasswordService();
+                }
+
+                return ChangePasswordService.instance;
+            }
+
+        /* Function to change password:
+        ========================================================================*/
+            public async changePassword(newPassword: string, accessToken: string): Promise<string> {
+                const { error } = await this.supabase.auth.updateUser(accessToken, {
+                    password: newPassword
+                });
+
+                if (error) throw new Error(error.message);
+
+                return "Password changed successfully!";
+            }
+    }
