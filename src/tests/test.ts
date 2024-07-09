85b61bf28de51ade5a7ee2e648d70af20a072069
Guilherme Henrique <a226881906@fumec.edu.br>
2024-07-09 01:04:05 -0300

    Delete: Deleting Test File With Error

--- src/tests/test.ts
+++
@@ -1,6 +0,0 @@
-import { SupabaseService } from './services/SupabaseService';
-import { AdministratorController } from './controllers/AdministratorController'; // Import the 'Supabase' module
-
-const controller = new AdministratorController(); // Create a new instance of the 'AdministratorController' class
-
-controller.sendAdminPass("icplatform.fumec@gmail.com", "1234567890");
